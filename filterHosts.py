b8c77391f7b2b19d5353d640fd9fe77bb1562812
Den Raskovalov <denbox@gmail.com>
2015-10-24 18:26:18 -0700

    fix filtration

--- filterHosts.py
+++ filterHosts.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+N = 1000
+
 fIn = open("2015-10-01-False-TopHosts2")
 hosts = []
 for line in fIn:
@@ -9,13 +11,13 @@ for line in fIn:
 hosts = sorted(hosts, key=lambda x: -x[1])
 
 sHosts = set()
-for i in range(5000):
+for i in range(N):
     sHosts.add(hosts[i][0])
     if i < 10:
         print(hosts[i][0])
 
 fData = open("hosts.txt")
-fOut = open("hosts.5000.txt", "w")
+fOut = open("hosts.%d.txt" % N, "w")
 for line in fData:
     if line.split()[0] in sHosts:
         print(line.strip(), file=fOut)
